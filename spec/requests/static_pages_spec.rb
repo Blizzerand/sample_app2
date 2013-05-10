dc9b2337618bb0ead7b1b75422fb7e9e7c3b8bf6
SanTrans <anon.wiki04@gmail.com>
2013-05-10 07:34:04 +0530

    Finish Desigining

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,39 +5,39 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text =>'Sample App')
     end
     it "should have the base title" do
-    	visit '/static_pages/home'
+    	visit root_path
     	page.should have_selector('title', :text => "Ruby on Rails Sample App")
     end
 	  it "should not have a custom page title" do
-	  	visit '/static_pages/home'
-	  	page.should_not have_selector('title', :text=> "Home")
+	  	visit root_path
+	  	page.should_not have_selector('title', :text=> "| Home")
 	  end
 	end
     
   describe "Help Page" do
   	
   	it "should have the content 'Help Page'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_selector('h1', :text=> 'Help')
   	end
   	it "should have the title 'Help'" do
-  		visit '/static_pages/help'
-  		page.should have_selector('title', :text => "Ruby on Rails Sample App | Help")
+  		visit help_path
+      page.should have_selector('title', :text => "Ruby on Rails Sample App | Help")
   	end
   end
   
   describe "About Us Page"  do
   	
   	it "should have the content 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_selector('h1', :text=>'About Us')
   	end
   	it "should have the title 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_selector('title', :text => "Ruby on Rails Sample App | About Us")
     end 
   end
@@ -45,12 +45,12 @@ describe "Static pages" do
   describe "Contact Us Page" do
   	
   	it "should have content 'Contact Us'" do
-  		visit '/static_pages/contact'
+  		visit contact_path
   		page.should have_selector('h1', :text => 'Contact Us')
   	end
 	 
 	 it "should have the title 'Contact Us'" do
-	 		visit '/static_pages/contact'
+	 		visit contact_path
 	 		page.should have_selector('title', :text => 'Ruby on Rails Sample App | Contact Us')
 	 		end 
   	end
